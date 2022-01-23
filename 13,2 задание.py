b749c79522353e646a19e73c399d8c8f3f18aab7
Tatianakarelova <andrey.333.k@yandex.ru>
2022-01-23 22:19:58 +0300

    проверка эквивалентности

--- 13,2 задание.py
+++ 13,2 задание.py
@@ -1,2 +1,11 @@
-print('5' in str(123456789))
-print (('3'and'7') in str(789456123))
+a = [1, 2, 3]
+print(id(a))
+b = a
+print(id(b))
+print(a is b)
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(a == b)
+print(a is b)
+print(a is None)
